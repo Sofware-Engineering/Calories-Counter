4c0c191cbd05886c1147b5f1b4ba11038630415e
anastasiia-dubrovska <nastja.dubrovska@gmail.com>
2025-05-19 23:39:41 +0300

    created architecture of project

---
+++ CaloriesCounterSolution/CaloriesCounter.BLL/ProductService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using CaloriesCounter.DAL;
+using CaloriesCounter.Entities;
+
+namespace CaloriesCounter.BLL
+{
+    public class ProductService
+    {
+        private readonly ProductRepository _repository;
+
+        public ProductService(string connectionString)
+        {
+            _repository = new ProductRepository(connectionString);
+        }
+
+        public List<Product> GetAllProducts() => _repository.GetAll();
+
+        public void AddProduct(Product product) => _repository.Add(product);
+    }
+}
